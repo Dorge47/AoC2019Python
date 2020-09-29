ed6a8b2e005ee50d020949668181e2ced0340561
Dorge47 <Dorge47@users.noreply.github.com>
2020-09-29 00:59:48 -0700

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -1,5 +1,6 @@
 inputRange = [256310, 732736]
 pwdList = []
+pwdList2 = []
 
 
 def verifyNum(numToVerify):
@@ -18,7 +19,35 @@ def verifyNum(numToVerify):
     return repeats
 
 
+def verifyNum2(numToVerify):
+    repeats = False
+    n = str(numToVerify)
+    if not len(n) == 6:
+        return False
+    for i in range(0, len(n)):
+        if i == 0:
+            continue
+        else:
+            if n[i] < n[i-1]:
+                return False
+        compareNum = int(n[i])
+        occurences = 1
+        for j in range(0, len(n)):
+            if i == j:
+                continue
+            else:
+                if int(n[j]) == compareNum:
+                    occurences += 1
+        if occurences == 2:
+            repeats = True
+    return repeats
+
+
 for i in range(inputRange[0], (inputRange[1]+1)):
     if verifyNum(i):
         pwdList.append(i)
 print(len(pwdList))
+for i in range(inputRange[0], (inputRange[1]+1)):
+    if verifyNum2(i):
+        pwdList2.append(i)
+print(len(pwdList2))
